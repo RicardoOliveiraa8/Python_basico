2c09ad2af9ccd691d390908bab8a88b1a9c3fa84
RicardoOliveiraa8 <136482484+RicardoOliveiraa8@users.noreply.github.com>
2023-07-23 05:27:14 -0300

    Testando

---
+++ Projeto.py
@@ -0,0 +1,10 @@
+lista = [1,2,3,4,5,6,7,8,9,10]
+
+def Maior(var):
+    maior = var [0]
+    for item in lista:
+        item > maior
+        maior = item
+        print('O Maior número da lista é: ', Maior)
+        
+Maior(Maior)
