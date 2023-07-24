c8ce9744ffeea30196b54a924109f35c392a93ca
RicardoOliveiraa8 <136482484+RicardoOliveiraa8@users.noreply.github.com>
2023-07-24 03:03:08 -0300

    Cadastro de clientes

--- Projeto.py
+++ Projeto.py
@@ -2,9 +2,9 @@ lista = [1,2,3,4,5,6,7,8,9,10]
 
 def Maior(var):
     maior = var [0]
-    for item in lista:
-        item > maior
-        maior = item
+    for numero in lista:
+        numero > maior
+        maior = numero
         print('O Maior número da lista é: ', Maior)
         
 Maior(Maior)
